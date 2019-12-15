5b08cd8a1810d571efb2ed79a6b9f5e0678aa81d
Maciej Niedzielski <mniedzielski@sigma.ug.edu.pl>
2019-12-15 21:34:16 +0100

    feat: connect publishers view to controller/database

--- Controllers/Publishers.cs
+++ Controllers/Publishers.cs
@@ -1,13 +1,24 @@
+using System.Linq;
+using BoardGames.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BoardGames.Controllers
 {
     public class Publishers : Controller
     {
+        
+        private readonly AppDbContext _dbContext;
+        
+        public Publishers(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         [Route("publishers")]
         public IActionResult Index()
         {
-            return View();
+            var publishers = _dbContext.Publisher.ToList();
+            return View(publishers);
         }
     }
 }
