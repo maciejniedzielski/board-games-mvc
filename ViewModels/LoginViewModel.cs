592a03e7c2e746a676c00bb2d6e6fecf0df0c441
Maciej Niedzielski <mniedzielski@sigma.ug.edu.pl>
2020-01-12 16:38:58 +0100

    feat: improve auth views

--- ViewModels/LoginViewModel.cs
+++ ViewModels/LoginViewModel.cs
@@ -5,10 +5,12 @@ namespace BoardGames.ViewModels
     public class LoginViewModel
     {
         [Required]
+        [Display(Name = "Email")]
         [EmailAddress]
         public string Email { get; set; }
         
         [Required]
+        [Display(Name = "Password")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
         
--- ViewModels/RegisterViewModel.cs
+++ ViewModels/RegisterViewModel.cs
@@ -5,14 +5,17 @@ namespace BoardGames.ViewModels
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "Email")]
         [EmailAddress]
         public string Email { get; set; }
         
         [Required]
+        [Display(Name = "Password")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
         
         [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "Given passwords do not match.")]
         public string ConfirmPassword { get; set; }
     }
